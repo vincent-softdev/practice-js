cf8d6b50759d4aeb33879610567e70f8ace1c5b1
vincent-softdev <vinh.ly.softdev@gmail.com>
2025-03-31 00:06:51 +1030

    timeout cancelation

--- LeetCode/Practicing/timeout_cancelation.js
+++ LeetCode/Practicing/timeout_cancelation.js
@@ -1,19 +1,23 @@
-function delayedExecution(fn, args, t, cancelTimeMs) {
-  let timerId;
+var cancellable = function(fn, args, t) {
+  // start timer
+  const start = Date.now();
+  // define the response
+  let log = []
 
-  // Schedule the execution of fn after t milliseconds
-  timerId = setTimeout(() => {
-      fn(...args);
+  // Now fn will happen after t time. This run outside of thread so it will start counting 
+  // from this line
+  let timerId = setTimeout(() => {
+      const result = fn(...args);
+      log.push({time: Date.now() - start, returned: result})
   }, t);
 
-  // Define the cancel function
+  // then now, if the cancelFn run but the timerId still waiting, then cancel task
   const cancelFn = () => {
       clearTimeout(timerId);
-      console.log('Execution cancelled');
-  };
+  }
 
-  // Automatically call cancelFn after cancelTimeMs
-  setTimeout(cancelFn, cancelTimeMs);
+  // We run cancelFn after cancelTimeMs
+  cancelFn.log = log;
 
-  return cancelFn;
-}
+  return cancelFn
+};
