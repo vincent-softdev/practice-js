980c8d987409c1a60fba5081f0c7369fcfcf2238
vincent-softdev <vinh.ly.softdev@gmail.com>
2025-04-03 23:35:31 +1030

    update binary search

---
+++ algorithm/binary_search.js
@@ -0,0 +1,22 @@
+// binary search
+const binary_search = (array, target) => {
+  let left = 0
+  let right = array.length - 1
+  // let middle position
+  let middle = Math.floor((left + right) / 2)
+
+  while(array[middle] !== target && left <= right) {
+    if(target < array[middle]) {
+      // move to the left
+      right = middle - 1
+    } else {
+      // move to the right
+      left = middle + 1
+    }
+    // update the middle
+    middle = Math.floor((left + right) / 2)
+  }
+  return array[middle] === target ? middle : -1
+}
+
+console.log(binary_search([1, 2, 3, 4, 5], 3))
